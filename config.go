7ffee0f2906da7d0cfeddeb5c72b420f1d722049
Martin Roed <msroed@gmail.com>
2023-05-09 12:40:57 +0200

    feat: add more regions

--- config.go
+++ config.go
@@ -7,7 +7,7 @@ const (
 
 	SENTRY_DSN = "https://aceadcbf56f14ef9a7fe76b0db5d7351@o1000232.ingest.sentry.io/4504735637176320"
 
-	SENTRY_TRACES_SAMPLE_RATE = 0.1
+	SENTRY_TRACES_SAMPLE_RATE = 0.05
 )
 
 var REGION_LOOKUP_MAP = map[string]string{
@@ -17,4 +17,9 @@ var REGION_LOOKUP_MAP = map[string]string{
 	"europe-west9":      "od",
 	"europe-west4":      "ez",
 	"europe-west8":      "oc",
+	"europe-west12":     "og",
+	"europe-west2":      "nw",
+	"europe-west3":      "ey",
+	"europe-west6":      "oa",
+	"europe-central2":   "lm",
 }

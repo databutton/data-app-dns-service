5308b1beda357550f6ff596897421e03683730d3
Martin Sandve Alnæs <msa@databutton.io>
2024-04-10 14:02:36 +0200

    remove unused code

--- config.go
+++ config.go
@@ -9,18 +9,3 @@ const (
 
 	SENTRY_TRACES_SAMPLE_RATE = 0.05
 )
-
-// TODO: After migrating to appbutlers we can get rid of this
-var REGION_LOOKUP_MAP = map[string]string{
-	"europe-west1":      "ew",
-	"europe-north1":     "lz",
-	"europe-southwest1": "no",
-	"europe-west9":      "od",
-	"europe-west4":      "ez",
-	"europe-west8":      "oc",
-	"europe-west12":     "og",
-	"europe-west2":      "nw",
-	"europe-west3":      "ey",
-	"europe-west6":      "oa",
-	"europe-central2":   "lm",
-}
